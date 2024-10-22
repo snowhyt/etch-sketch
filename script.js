59c7d307f1d5167609a9915d0a3816b3d631f350
snowhyt <oliverglorioso.44@gmail.com>
2024-10-22 19:03:41 +0800

    firs commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const GRIDESIDE= 600;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.getElementById('sketch-area');
+sketchArea.style.width = `${GRIDESIDE}px`;
+sketchArea.style.height =`${GRIDESIDE}px`;
+
+
+function createGridCells() {
+    for(let i = 0; i < (rows * cols); i++){
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = `${(GRIDESIDE / cols)-2}`;
+        gridCell.style.height = `${(GRIDESIDE / rows)-2}`;
+
+    }
+}
