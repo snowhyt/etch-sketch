222b63416ece5dc2d135f84d2668325ce7043d29
snowhyt <oliverglorioso.44@gmail.com>
2024-10-27 12:07:38 +0800

    add toggle buttons

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const GRIDSIDE= 600;
-const squaresPerSide = 16;
+const gridWidth =getComputedStyle(document.body).getPropertyValue("--grid-width")
+const accentColor =getComputedStyle(document.body).getPropertyValue("--accent-color")
+const inactiveColor =getComputedStyle(document.body).getPropertyValue("--inactive-color")
+
+
+const container = document.querySelector(".container");
 
 const sketchArea = document.getElementById("sketch-area");
 const sliderContainer = document.getElementById("slider-container");
@@ -7,23 +11,40 @@ const slider = document.getElementById("slider");
 const sliderValue = document.getElementById("slider-value");
 
 
+const gridToggle = document.getElementById("grid-toggle");
 
-sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
-sketchArea.style.width = sketchArea.style.height =`${GRIDSIDE}px`;
+// sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
+// sketchArea.style.width = sketchArea.style.height =`${GRIDSIDE}px`;
 
+let squaresPerSide = 16;
+let gridVisible = false;
+
+function toggleGrid(){
+    gridVisible = gridVisible ? false : true;
+    gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
+
+    removeGridCells();
+    createGridCells();
+}
 
 function setBackgroundColor(){
     this.style.backgroundColor = "pink";
 }
 
-function createGridCells(squaresPerSide) {
+function createGridCells() {
 
     const numOfSquares = (squaresPerSide * squaresPerSide);
-    const widthHeight = `${(GRIDSIDE/squaresPerSide)-2}px`; 
+    //const widthHeight = `${(gridWidth/squaresPerSide)-2}px`; 
     for(let i = 0; i < numOfSquares; i++){
         const gridCell = document.createElement("div");
-        gridCell.style.width = widthHeight;
-        gridCell.style.height = widthHeight;
+      
+        if(gridVisible){
+            widthHeight = `${(parseInt(gridWidth)/squaresPerSide)-2}px`;
+            gridCell.style.border = "1px solid whitesmoke";
+        } else if (!gridVisible){
+            widthHeight = `${(parseInt(gridWidth)/squaresPerSide)}px`;
+        }
+        gridCell.style.width = gridCell.style.height = widthHeight;
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
@@ -47,4 +68,7 @@ slider.oninput = function(){
 }
 
 
-createGridCells(squaresPerSide);
+createGridCells();
+
+
+//29:57 
